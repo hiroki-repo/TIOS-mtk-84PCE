831d4c54170775e800ddcac766c37711f25a151b
hiroki-repo <hiroki7v11@gmail.com>
2024-07-11 19:51:59 +0900

    added some of syscalls!

--- main.c
+++ main.c
@@ -5,8 +5,12 @@
 #include "Stddef.h"
 #include "String.h"
 extern char* em180mem;
+extern char* em180_sa4resv;
+unsigned int stackarearem = 3072;
 
-char context[16][3*16];
+#define GT_TASKMAX 15
+
+char context[(GT_TASKMAX + 1)][3*16];
 
 typedef unsigned int SVCID;
 
@@ -18,6 +22,7 @@ typedef unsigned int PRI;
 typedef unsigned int SIZE;
 typedef unsigned int ATR;
 typedef signed int ER;
+typedef signed int ER_UINT;
 
 #define E_OK 0
 #define E_SYS -5
@@ -59,7 +64,9 @@ extern ER unsup_svc(void){
 }
 
 extern ER cre_tsk(ID tskid,T_CTSK* pk_ctsk){
+	if (tskid==0){tskid = pidnow;}
 	if (context[tskid][3*13]==0x00){
+		if (pk_ctsk->stk == NULL){pk_ctsk->stk = malloc(pk_ctsk->stksz); if (pk_ctsk->stk == NULL){return E_NOMEM;}}
 		context[tskid][3*13] = 0x5B;
 		(*(FP*)&context[tskid][3*11]) = pk_ctsk->task;
 		(*(VP*)&context[tskid][3*12]) = pk_ctsk->stk;
@@ -71,11 +78,90 @@ extern ER cre_tsk(ID tskid,T_CTSK* pk_ctsk){
 }
 
 extern ER del_tsk(ID tskid){
-	if (tskid>=16){return E_ID;}
-	if (tskid==pidnow){return E_OBJ;}
+	if (tskid==0){tskid = pidnow;}
+	if (tskid>=(GT_TASKMAX + 1)){return E_ID;}
+	if (((*(T_CTSK**)&context[tskid][3*15])->tskatr & 2) == 0){return E_OBJ;}
 	if (context[tskid][3*13]!=0x00){
 		context[tskid][3*13] = 0;
 		(*(T_CTSK**)&context[tskid][3*15]) = 0;
+		return E_OK;
+	}
+	return E_NOEXS;
+}
+
+extern ER act_tsk(ID tskid){
+	if (tskid==0){tskid = pidnow;}
+	if (tskid>=(GT_TASKMAX + 1)){return E_ID;}
+	if (context[tskid][3*13]!=0x00){
+		T_CTSK* pk_ctsk = (*(T_CTSK**)&context[tskid][3*15]);
+		pk_ctsk->tskatr &= ~(2);
+		return E_OK;
+	}
+	return E_NOEXS;
+}
+
+extern ER_UINT can_act(ID tskid){
+	return E_NOSPT;
+}
+
+extern void ext_tsk(void){
+		T_CTSK* pk_ctsk = (*(T_CTSK**)&context[pidnow][3*15]);
+		pk_ctsk->tskatr |= 2;
+}
+
+extern ER sta_tsk(ID tskid, VP_INT stacd){
+	if (tskid==0){tskid = pidnow;}
+	if (tskid>=(GT_TASKMAX + 1)){return E_ID;}
+	if (((*(T_CTSK**)&context[tskid][3*15])->tskatr & 2) == 0){return E_OBJ;}
+	if (context[tskid][3*13]!=0x00){
+		T_CTSK* pk_ctsk = (*(T_CTSK**)&context[tskid][3*15]);
+		pk_ctsk->tskatr &= ~(2);
+		pk_ctsk->stk -= 3;
+		(*(VP_INT*)(pk_ctsk->stk)) = stacd;
+		pk_ctsk->stk -= 3;
+		(*(VP_INT*)(pk_ctsk->stk)) = ((VP_INT)&ext_tsk);
+		return E_OK;
+	}
+	return E_NOEXS;
+}
+
+extern void exd_tsk(){
+	ext_tsk();
+	del_tsk(0);
+}
+
+extern ER ter_tsk(ID tskid){
+	if (tskid==0){tskid = pidnow;}
+	if (tskid == pidnow){return E_ILUSE;}
+	if (((*(T_CTSK**)&context[tskid][3*15])->tskatr & 2) != 0){return E_OBJ;}
+	if (tskid>=(GT_TASKMAX + 1)){return E_ID;}
+	if (context[tskid][3*13]!=0x00){
+		T_CTSK* pk_ctsk = (*(T_CTSK**)&context[tskid][3*15]);
+		pk_ctsk->tskatr &= ~(2);
+		return E_OK;
+	}
+	return E_NOEXS;
+}
+
+extern ER chg_pri(ID tskid,PRI tskpri){
+	if (tskid==0){tskid = pidnow;}
+	if (tskid>=(GT_TASKMAX + 1)){return E_ID;}
+	if (((*(T_CTSK**)&context[tskid][3*15])->tskatr & 2) != 0){return E_OBJ;}
+	if (context[tskid][3*13]!=0x00){
+		T_CTSK* pk_ctsk = (*(T_CTSK**)&context[tskid][3*15]);
+		pk_ctsk->itskpri = tskpri;
+		return E_OK;
+	}
+	return E_NOEXS;
+}
+
+extern ER get_pri(ID tskid,PRI *p_tskpri){
+	if (tskid==0){tskid = pidnow;}
+	if (tskid>=(GT_TASKMAX + 1)){return E_ID;}
+	if (((*(T_CTSK**)&context[tskid][3*15])->tskatr & 2) != 0){return E_OBJ;}
+	if (context[tskid][3*13]!=0x00){
+		T_CTSK* pk_ctsk = (*(T_CTSK**)&context[tskid][3*15]);
+		*p_tskpri = pk_ctsk->itskpri;
 		return E_OK;
 	}
 	return E_NOEXS;
@@ -89,7 +175,7 @@ extern ER syscallhandler(SVCID syscallid,VP prm_0,VP prm_1,VP prm_2,VP prm_3,VP 
 
 extern int main(){
 	while(1){
-		for(pidnow=0;pidnow<16;pidnow++){
+		for(pidnow=0;pidnow<(GT_TASKMAX + 1);pidnow++){
 			if (context[pidnow][3*13]!=0x00){
 				memcpy(&em180mem,context[pidnow],sizeof(context[pidnow]));
 				em180();
